d0fb7e0271ffa78702ce1c5cad44692fe2204a94
王佳怡 <wangjiayi.p@gmail.com>
2015-11-09 21:48:27 +0800

    some theme

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -13,52 +13,50 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html lang="<?= Yii::$app->language ?>" class="m js cssanimations">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <style type="text/css">
+    .main-header {    
+        z-index: 100;
+        background: #0e90d2;
+        color: #FFF;    
+        position: relative;    
+        box-shadow: 0 0 3px rgba(0,0,0,.15);
+        font-size: 1.6rem;
+        height: 45px;
+        padding: 5px;
+    }
+    .main-header h1 {
+        text-align: center;
+        margin-top: 0;
+    }
+    #btn-back {
+        position: absolute;
+        margin-top: 6px;
+    }
+    .main-post {
+        float: right;
+    }
+    </style>
 </head>
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap">
-<!--     <?php
-    NavBar::begin([
-        'brandLabel' => 'My Company',
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ?
-                ['label' => 'Login', 'url' => ['/site/login']] :
-                [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ],
-        ],
-    ]);
-    NavBar::end();
-    ?> -->
-
-    <div class="container">
+<div class="am-page" id="mobile-index">
+<header class="main-header"><?= Html::a('发帖', ['create'], ['class' => 'btn btn-success main-post']) ?><span class="am-icon-chevron-left" id="btn-back"></span><h1><?= Html::encode($this->title) ?></h1></header>
+    <!-- <div class="container"> -->
        <!-- <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>-->
         <?= $content ?>
-    </div>
+    <!-- </div> -->
 </div>
-
+<!-- </div> -->
 <!-- <footer class="footer">
     <div class="container">
         <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
--- views/post/index.php
+++ views/post/index.php
@@ -6,16 +6,13 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Posts';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = '附近的公告';
+// $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="post-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <!-- <h1><?= Html::encode($this->title) ?></h1> -->
 
-    <p>
-        <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
     <ul class="am-list am-list-static am-list-border">
     <?php 
     $models = array_values($dataProvider->getModels());
