12f74bbd8ab31a80607a4060bfbd013208395c73
王佳怡 <wangjiayi.p@gmail.com>
2015-11-25 00:04:41 +0800

    some fix

--- views/post/view.php
+++ views/post/view.php
@@ -254,7 +254,7 @@ function toUtf8(str) {
                var res = $.parseJSON(info);
 
                var sourceLink = domain + res.key; //获取上传成功后的文件的Url
-               $(".img-box").append('<img src="'+sourceLink+'?imageView2/1/w/100/q/30">');
+               $("#container .img-box").append('<img src="'+sourceLink+'?imageView2/1/w/100/q/30">');
                $("#data-img").val($("#data-img").val()+res.key+',');
         },
         'Error': function(up, err, errTip) {
@@ -275,3 +275,7 @@ function toUtf8(str) {
     }
 });
 </script>
+<div style="display:none" class="img_cover">
+    <div class="am-dimmer am-active" data-am-dimmer="" id="am-dimmer-d2qaa" style="display: block;"></div>
+    <div class="am-modal am-modal-loading am-modal-no-btn am-modal-active" tabindex="-1" id="my-modal-loading" style="display: block; margin-top: -50.5px;"><div class="am-modal-dialog"><div class="am-modal-hd">上传中......</div><div class="am-modal-bd"><span class="am-icon-spinner am-icon-spin"></span></div></div></div>  
+</div>
